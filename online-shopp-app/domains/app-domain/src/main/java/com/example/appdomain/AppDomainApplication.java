d1e724e616cce2c0d8b4004db196dce567f5e29e
RShbz <remzisahbaz@outlook.com>
2022-05-31 12:51:14 +0300

    first commit

---
+++ online-shopp-app/domains/app-domain/src/main/java/com/example/appdomain/AppDomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDomainApplication.class, args);
+    }
+
+}
---
+++ online-shopp-app/domains/app-domain/src/test/java/com/example/appdomain/AppDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
